696f58298468fc77aec2e875e2c451328788e04d
VIkas Kashyap <vikas965046@gmail.com>
2025-11-06 22:33:03 +0530

    Create FindNumberOf1Bits.cpp

---
+++ Codes/Questions/Bit Manipulation/FindNumberOf1Bits.cpp
@@ -0,0 +1,25 @@
+/*
+Given a positive integer n, write a function that returns the number of set bits in its binary representation 
+(also known as the Hamming weight).
+
+Example 1:
+
+Input: n = 11
+
+Output: 3
+
+Explanation:
+
+The input binary string 1011 has a total of three set bits.
+*/
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    cout<<__builtin_popcount(n);
+
+    return 0;
+}
